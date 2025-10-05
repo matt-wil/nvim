d863c9b3a3084377ced05b9827240a8aa9aac03c
Matt Williams <matthew.steven.williams@gmail.com>
2025-10-05 12:48:03 +0200

    feat(keymaps): add in keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,228 +1,171 @@
-return {}
--- return {
---     'neovim/nvim-lspconfig',
---     dependencies = {
---         'williamboman/mason.nvim',
---         'williamboman/mason-lspconfig.nvim',
---         -- Autocompletion
---         'hrsh7th/nvim-cmp',
---         'hrsh7th/cmp-buffer',
---         'hrsh7th/cmp-path',
---         'saadparwaiz1/cmp_luasnip',
---         'hrsh7th/cmp-nvim-lsp',
---         'hrsh7th/cmp-nvim-lua',
---         -- Snippets
---         'L3MON4D3/LuaSnip',
---         'rafamadriz/friendly-snippets',
---     },
---     config = function()
---         local autoformat_filetypes = {
---             "lua",
---         }
---         -- Create a keymap for vim.lsp.buf.implementation
---         vim.api.nvim_create_autocmd('LspAttach', {
---             callback = function(args)
---                 local client = vim.lsp.get_client_by_id(args.data.client_id)
---                 if not client then return end
---                 if vim.tbl_contains(autoformat_filetypes, vim.bo.filetype) then
---                     vim.api.nvim_create_autocmd("BufWritePre", {
---                         buffer = args.buf,
---                         callback = function()
---                             vim.lsp.buf.format({
---                                 formatting_options = { tabSize = 4, insertSpaces = true },
---                                 bufnr = args.buf,
---                                 id = client.id
---                             })
---                         end
---                     })
---                 end
---             end
---         })
---
---         -- Add borders to floating windows
---         vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
---             vim.lsp.handlers.hover,
---             { border = 'rounded' }
---         )
---         vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
---             vim.lsp.handlers.signature_help,
---             { border = 'rounded' }
---         )
---
---         -- Configure error/warnings interface
---         vim.diagnostic.config({
---             virtual_text = true,
---             severity_sort = true,
---             float = {
---                 style = 'minimal',
---                 border = 'rounded',
---                 header = '',
---                 prefix = '',
---             },
---             signs = {
---                 text = {
---                     [vim.diagnostic.severity.ERROR] = '✘',
---                     [vim.diagnostic.severity.WARN] = '▲',
---                     [vim.diagnostic.severity.HINT] = '⚑',
---                     [vim.diagnostic.severity.INFO] = '»',
---                 },
---             },
---         })
---
---         local lspconfig_defaults = require('lspconfig').util.default_config
---         lspconfig_defaults.capabilities = vim.tbl_deep_extend(
---             'force',
---             lspconfig_defaults.capabilities,
---             require('cmp_nvim_lsp').default_capabilities()
---         )
---
---         -- This is where you enable features that only work
---         -- if there is a language server active in the file
---         vim.api.nvim_create_autocmd('LspAttach', {
---             callback = function(event)
---                 local opts = { buffer = event.buf }
---
---                 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
---                 vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
---                 vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
---                 vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
---                 vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
---                 vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
---                 vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
---                 vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
---                 vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
---                 vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
---                 vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
---             end,
---         })
---
---         require('mason').setup({})
---         require('mason-lspconfig').setup({
---             ensure_installed = {
---                 "lua_ls",
---                 "intelephense",
---                 "ts_ls",
---                 "eslint",
---                 "pyright",
---             },
---             handlers = {
---                 function(server_name)
---                     if server_name == "luals" then return end -- avoid starting with {}
---                     require('lspconfig')[server_name].setup({})
---                 end,
---
---                 lua_ls = function()
---                     require('lspconfig').luals.setup({
---                         settings = {
---                             Lua = {
---                                 runtime = {
---                                     version = 'LuaJIT',
---                                 },
---                                 diagnostics = {
---                                     globals = { 'vim' },
---                                 },
---                                 workspace = {
---                                     library = { vim.env.VIMRUNTIME },
---                                 },
---                             },
---                         },
---                     })
---                 end,
---             },
---         })
---
---         local cmp = require('cmp')
---
---         require('luasnip.loaders.from_vscode').lazy_load()
---
---         vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
---
---         cmp.setup({
---             preselect = 'item',
---             completion = {
---                 completeopt = 'menu,menuone,noinsert'
---             },
---             window = {
---                 documentation = cmp.config.window.bordered(),
---             },
---             sources = {
---                 { name = 'path' },
---                 { name = 'nvim_lsp' },
---                 { name = 'buffer',  keyword_length = 3 },
---                 { name = 'luasnip', keyword_length = 2 },
---             },
---             snippet = {
---                 expand = function(args)
---                     require('luasnip').lsp_expand(args.body)
---                 end,
---             },
---             formatting = {
---                 fields = { 'abbr', 'menu', 'kind' },
---                 format = function(entry, item)
---                     local n = entry.source.name
---                     if n == 'nvim_lsp' then
---                         item.menu = '[LSP]'
---                     else
---                         item.menu = string.format('[%s]', n)
---                     end
---                     return item
---                 end,
---             },
---             mapping = cmp.mapping.preset.insert({
---                 -- confirm completion item
---                 ['<CR>'] = cmp.mapping.confirm({ select = false }),
---
---                 -- scroll documentation window
---                 ['<C-f>'] = cmp.mapping.scroll_docs(5),
---                 ['<C-u>'] = cmp.mapping.scroll_docs(-5),
---
---                 -- toggle completion menu
---                 ['<C-e>'] = cmp.mapping(function(fallback)
---                     if cmp.visible() then
---                         cmp.abort()
---                     else
---                         cmp.complete()
---                     end
---                 end),
---
---                 -- tab complete
---                 ['<Tab>'] = cmp.mapping(function(fallback)
---                     local col = vim.fn.col('.') - 1
---
---                     if cmp.visible() then
---                         cmp.select_next_item({ behavior = 'select' })
---                     elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
---                         fallback()
---                     else
---                         cmp.complete()
---                     end
---                 end, { 'i', 's' }),
---
---                 -- go to previous item
---                 ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
---
---                 -- navigate to next snippet placeholder
---                 ['<C-d>'] = cmp.mapping(function(fallback)
---                     local luasnip = require('luasnip')
---
---                     if luasnip.jumpable(1) then
---                         luasnip.jump(1)
---                     else
---                         fallback()
---                     end
---                 end, { 'i', 's' }),
---
---                 -- navigate to the previous snippet placeholder
---                 ['<C-b>'] = cmp.mapping(function(fallback)
---                     local luasnip = require('luasnip')
---
---                     if luasnip.jumpable(-1) then
---                         luasnip.jump(-1)
---                     else
---                         fallback()
---                     end
---                 end, { 'i', 's' }),
---             }),
---         })
---     end
--- }
+-- This is the complete configuration file with the updated nvim-lspconfig setup.
+
+return {
+	-- Installs LSP servers, formatters, and linters
+	{
+	    "williamboman/mason.nvim",
+		opts = {
+            -- This is the list of ALL tools to install with Mason.
+            -- Includes LSPs, linters, and formatters.
+			ensure_installed = {
+				-- LSPs
+				"vtsls",
+				"rust-analyzer",
+				"tailwindcss",
+				"cssls",
+				"terraformls",
+                "html", -- From your previous config
+                "yamlls", -- From your previous config
+                "lua-language-server", -- The package name for lua_ls
+
+				-- Linters & Formatters
+				"shellcheck",
+				"shfmt",
+                "stylua", -- From your previous config
+                "selene", -- From your previous config
+                "luacheck", -- From your a previous config
+			},
+		},
+	},
+	-- Bridges mason.nvim with nvim-lspconfig for automatic server setup
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			-- This is your list of servers that mason-lspconfig will ensure are installed.
+			ensure_installed = {
+				"lua_ls",
+				"rust_analyzer",
+				"vtsls",
+				"tailwindcss",
+				"cssls",
+				"html",
+				"yamlls",
+				"vtsls",
+				"terraformls",
+			},
+		},
+	},
+
+	-- The main LSP configuration
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		-- This config function is updated to use the new vim.lsp.config API.
+		config = function()
+			-- This function runs for each LSP server that attaches to a buffer.
+			local on_attach = function(client, bufnr)
+				-- --- Your Custom Keymaps ---
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover (Type/Docs)" })
+				vim.keymap.set("n", "gl", vim.diagnostic.open_float, { buffer = bufnr, desc = "Show Line Diagnostics" })
+				vim.keymap.set("n", "gd", function()
+					require("telescope.builtin").lsp_definitions({ reuse_win = false })
+				end, { buffer = bufnr, desc = "Goto Definition (Telescope)" })
+
+				-- --- Other Standard LSP Keymaps ---
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Go to Declaration" })
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "Go to References" })
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename Symbol" })
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
+			end
+
+			-- Standard capabilities for LSP servers.
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+			-- --- Your Custom Server Configurations ---
+			-- A table holding server-specific settings.
+			local servers = {
+				cssls = {},
+				tailwindcss = {},
+				vtsls = {
+					on_attach = function(client, bufnr)
+						on_attach(client, bufnr)
+
+						local root = require("lspconfig.util").root_pattern(".git")(vim.api.nvim_buf_get_name(bufnr))
+						if root and vim.fn.filereadable(root .. "/.prettierrc") > 0 then
+							-- If a prettier config is found, disable the vtsls formatter to prevent conflicts
+							client.server_capabilities.documentFormattingProvider = false
+							client.server_capabilities.documentRangeFormattingProvider = false
+						end
+					end,
+				},
+				html = {},
+				terraformls = {
+					filetypes = { "terraform", "tf", "tfvars" },
+				},
+				yamlls = {
+					settings = {
+						yaml = {
+							keyOrdering = false,
+						},
+					},
+				},
+				lua_ls = {
+					single_file_support = true,
+					settings = {
+						Lua = {
+							workspace = { checkThirdParty = false },
+							completion = { workspaceWord = true, callSnippet = "Both" },
+							hint = {
+								enable = true,
+								setType = false,
+								paramType = true,
+								paramName = "Disable",
+								semicolon = "Disable",
+								arrayIndex = "Disable",
+							},
+							doc = { privateName = { "^_" } },
+							type = { castNumberToInteger = true },
+							diagnostics = {
+								disable = { "incomplete-signature-doc", "trailing-space" },
+								groupSeverity = { strong = "Warning", strict = "Warning" },
+								groupFileStatus = {
+									ambiguity = "Opened",
+									await = "Opened",
+									codestyle = "None",
+									duplicate = "Opened",
+									global = "Opened",
+									luadoc = "Opened",
+									redefined = "Opened",
+									strict = "Opened",
+									strong = "Opened",
+									["type-check"] = "Opened",
+									unbalanced = "Opened",
+									unused = "Opened",
+								},
+								unusedLocalExclude = { "_*" },
+							},
+							format = { enable = false },
+						},
+					},
+				},
+			}
+
+			-- This loop sets up every server with your custom settings.
+			for server_name, server_config in pairs(servers) do
+				-- Merge the server-specific settings with our shared settings
+				local final_config = vim.tbl_deep_extend("force", {
+					on_attach = on_attach,
+					capabilities = capabilities,
+					inlay_hints = { enabled = false },
+				}, server_config)
+
+				-- The new way to configure and enable LSPs
+				vim.lsp.config(server_name, final_config)
+				vim.lsp.enable(server_name)
+			end
+		end,
+	},
+
+	-- Markview config, correctly configured to be lazy-loaded.
+	{
+		"OXY2DEV/markview.nvim",
+		ft = { "markdown" }, -- Only loads for markdown files
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+}
